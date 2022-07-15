375d899cb9aea0baca0770de01c5288eaeb564a2
Ethan Fung <EthanEFung@gmail.com>
2022-07-15 08:52:13 -0700

    burger decorator

---
+++ decorator/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Burger interface {
+	Ingredients() []string 
+	Price() float64
+}
+
+type BaseBurger struct {
+	Burger
+}
+func (h BaseBurger) Ingredients() []string {
+	return []string{}
+}
+func (h BaseBurger) Price() float64 {
+	return 10.27
+}
+
+type WithBun struct {
+	Burger
+	Type string
+	AdditionalCost float64
+}
+func (d WithBun) Ingredients() []string {
+	return append(d.Burger.Ingredients(), d.Type + " Bun")
+}
+
+type WithCheese struct {
+	Burger
+	Type string
+	AdditionalCost float64
+}
+func (d WithCheese) Ingredients() []string {
+	base := d.Burger.Ingredients()
+	return append(base, d.Type + " Cheese")
+}
+func (d WithCheese) Price() float64 {
+	return math.Floor( (d.Burger.Price() + 1.75) * 100 ) / 100
+}
+
+type WithPattie struct {
+	Burger
+	Type string
+	AdditionalCost float64
+}
+func (d WithPattie) Ingredients() []string{
+	return append(d.Burger.Ingredients(), d.Type + " Pattie")
+}
+func (d WithPattie) Price() float64 {
+	return math.Floor( (d.Burger.Price() + d.AdditionalCost) * 100 ) / 100
+}
+
+func main() {
+	base := &BaseBurger{}
+
+	hamburger := &WithPattie{&WithBun{base, "Brioche", 0}, "Beef", 0}
+	cheeseBurger := &WithCheese{hamburger, "American", 0.75}
+	veggieBurger := &WithPattie{&WithBun{base, "Brioche", 0}, "Impossible", 2.10}
+	proteinStyle := &WithCheese{&WithPattie{&WithBun{base, "Lettuce", 0}, "Beef", 0}, "American", 0}
+
+	fmt.Println("hamburger", hamburger.Ingredients(), hamburger.Price())
+	fmt.Println("cheese burger", cheeseBurger.Ingredients(), cheeseBurger.Price())
+	fmt.Println("veggie burger", veggieBurger.Ingredients(), veggieBurger.Price())
+	fmt.Println("protein style", proteinStyle.Ingredients(), proteinStyle.Price())
+}
