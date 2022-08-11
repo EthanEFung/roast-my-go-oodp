6a311bdc71a8dbbdc2b0ff301d0c4c224eb12f21
Ethan Fung <EthanEFung@gmail.com>
2022-08-11 15:38:13 -0700

    adapter pattern documents

--- adapter/main.go
+++ adapter/main.go
@@ -7,9 +7,5 @@ func main() {
 	yamlService := &yamlService{}
 	
 	a := &adapter{service: yamlService}
-<<<<<<< HEAD
-=======
-	a.convert()
->>>>>>> b9317a09d281e91a5c996d0f38f35734ed52e827
 	a.exportJSON([]byte{})
 }
