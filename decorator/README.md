50b11ad1068773ca2f933ba54f19fa479a6bb9b7
Ethan Fung <EthanEFung@gmail.com>
2022-08-19 11:31:10 -0700

    decorator documentation

---
+++ decorator/README.md
@@ -0,0 +1,10 @@
+# decorator
+
+decorator is a behavioral pattern that layers functionality on top of an objects
+existing methods. A decorator adheres to a base objects methods, and extends or
+modifies the behavior. Visit the `main.go` file to see an example and from the 
+terminal run 
+```
+go run main.go
+```
+in this directory to see the output of the main function.
--- decorator/main.go
+++ decorator/main.go
@@ -5,14 +5,18 @@ import (
 	"math"
 )
 
+/*
+	Burger in this example would be an existing interface that decorators will adhere to
+*/
 type Burger interface {
-	Ingredients() []string 
+	Ingredients() []string
 	Price() float64
 }
 
 type BaseBurger struct {
 	Burger
 }
+
 func (h BaseBurger) Ingredients() []string {
 	return []string{}
 }
@@ -20,43 +24,60 @@ func (h BaseBurger) Price() float64 {
 	return 10.27
 }
 
+/*
+	WithBun is a concrete decorator that takes in a base struct that adheres to the Burger interface
+*/
 type WithBun struct {
 	Burger
-	Type string
+	Type           string
 	AdditionalCost float64
 }
+
 func (d WithBun) Ingredients() []string {
-	return append(d.Burger.Ingredients(), d.Type + " Bun")
+	return append(d.Burger.Ingredients(), d.Type+" Bun")
 }
 
+/*
+	WithCheese is a concrete decorator that takes in a base struct that adheres to the Burger interface
+*/
 type WithCheese struct {
 	Burger
-	Type string
+	Type           string
 	AdditionalCost float64
-}
-func (d WithCheese) Ingredients() []string {
-	base := d.Burger.Ingredients()
-	return append(base, d.Type + " Cheese")
-}
-func (d WithCheese) Price() float64 {
-	return math.Floor( (d.Burger.Price() + 1.75) * 100 ) / 100
 }
 
+func (d WithCheese) Ingredients() []string {
+	base := d.Burger.Ingredients()
+	return append(base, d.Type+" Cheese")
+}
+func (d WithCheese) Price() float64 {
+	return math.Floor((d.Burger.Price()+1.75)*100) / 100
+}
+
+/*
+	WithPattie is a concrete decorator that takes in a base struct that adheres to the Burger interface
+*/
 type WithPattie struct {
 	Burger
-	Type string
+	Type           string
 	AdditionalCost float64
 }
-func (d WithPattie) Ingredients() []string{
-	return append(d.Burger.Ingredients(), d.Type + " Pattie")
+
+func (d WithPattie) Ingredients() []string {
+	return append(d.Burger.Ingredients(), d.Type+" Pattie")
 }
 func (d WithPattie) Price() float64 {
-	return math.Floor( (d.Burger.Price() + d.AdditionalCost) * 100 ) / 100
+	return math.Floor((d.Burger.Price()+d.AdditionalCost)*100) / 100
 }
 
 func main() {
 	base := &BaseBurger{}
 
+	/*
+		because all of these decorated objects adhere to the same interface as the base burger,
+		this allows us the ability to use the same methods while modifying the processes that
+		take place within those methods.
+	*/
 	hamburger := &WithPattie{&WithBun{base, "Brioche", 0}, "Beef", 0}
 	cheeseBurger := &WithCheese{hamburger, "American", 0.75}
 	veggieBurger := &WithPattie{&WithBun{base, "Brioche", 0}, "Impossible", 2.10}
